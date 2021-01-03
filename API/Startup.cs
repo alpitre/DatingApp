1322eb872e0f8ea5e4a22ed6de5f5aa4d6191b8c
Alain <alpeetre@hotmail.com>
2021-01-02 22:12:36 -0500

    End of Sectin 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,6 @@
-using API.Data;
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,12 +19,11 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<DataContext>(options =>
-      {
-        options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
-      });
+      services.AddApplicationServices(_config);
       services.AddControllers();
       services.AddCors();
+      services.AddIdentityServices(_config);
+
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -48,6 +46,7 @@ namespace API
 
       app.UseCors(x => x.AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+      app.UseAuthentication();
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
